8954aaa6d1271b257bf063e66197a99a183256d1
masa <rxe06602@nifty.com>
2021-02-11 16:48:55 +0900

    最終型

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
+LOGIN_REDIRECT_URL = '/'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'pn2j8$ek6$xwze$a^$sfb_jk2ftna1zja$8tdq97^f-okk%_e)'
 
